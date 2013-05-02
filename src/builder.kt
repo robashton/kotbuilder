beaaf228da22cc14172289657eb750eec32785af
Rob Ashton <robashton@codeofrob.com>
2013-05-02 13:32:50 +0100

    Initial commit

---
+++ src/builder.kt
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: robashton
+ * Date: 02/05/13
+ * Time: 13:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+class JsonBuilder {
+
+}
